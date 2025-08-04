11254a2e9da0a4c3ce04077a10feaf8df33b69a8
Kshitij0253 <ks904125@gmail.com>
2025-08-04 10:50:49 +0530

    4sum leetcode solution

--- ThreeSum.java
+++ ThreeSum.java
@@ -10,6 +10,21 @@ public class ThreeSum {
 
 //brute force 
 
+public List<List<Integer>> threeSum1(int[] nums){
+    List<List<Integer>> ans = new ArrayList<>();
+    Arrays.sort(nums);
+    for(int i=0;i<nums.length-2;i++){
+        for(int j=i+1;j<nums.length-1;j++){
+            for(int k=j+1;k<nums.length;k++){
+                if(nums[i]+nums[j]+nums[k]==0){
+                    ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                }
+            }
+        }
+    }
+    return ans;
+}
+
 
 
 
